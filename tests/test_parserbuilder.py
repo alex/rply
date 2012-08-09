f710e662ecc2ed5edef477d51ac88dcb19511b51
Alex Gaynor <alex.gaynor@gmail.com>
2012-08-08 19:58:00 -0700

    more tests, coverage++

--- tests/test_parserbuilder.py
+++ tests/test_parserbuilder.py
@@ -1,4 +1,7 @@
+import py
+
 from rply import ParserGenerator
+from rply.errors import ParserGeneratorError
 
 
 class TestParserBuilder(object):
@@ -16,3 +19,42 @@ class TestParserBuilder(object):
             1: {"$end": 0},
             2: {"$end": -1},
         }
+
+    def test_empty_production(self):
+        pg = ParserGenerator(["VALUE"])
+
+        @pg.production("main : values")
+        def main(p):
+            return p[0]
+
+        @pg.production("values : VALUE values")
+        def values_value(p):
+            return [p[0]] + p[1]
+
+        @pg.production("values :")
+        def values_empty(p):
+            return []
+
+        parser = pg.build()
+        assert parser.lr_table.lr_action == {
+            0: {"$end": -3, "VALUE": 3},
+            1: {"$end": 0},
+            2: {"$end": -1},
+            3: {"$end": -3, "VALUE": 3},
+            4: {"$end": -2},
+        }
+
+    def test_production_syntax_error(self):
+        pg = ParserGenerator([])
+        with py.test.raises(ParserGeneratorError):
+            pg.production("main VALUE")
+
+    def test_production_terminal_overlap(self):
+        pg = ParserGenerator(["VALUE"])
+
+        @pg.production("VALUE :")
+        def x(p):
+            pass
+
+        with py.test.raises(ParserGeneratorError):
+            pg.build()
