4dc6c5e2af9a6a3d394530bc96bf24872cd84010
Marco Gario <marco.gario@email.it>
2013-04-21 15:22:32 +0200

    Replaced __update_lineno__ with a simpler function to determine SourcePosition

--- rply/lexer.py
+++ rply/lexer.py
@@ -16,8 +16,6 @@ class LexerStream(object):
         self.lexer = lexer
         self.s = s
         self.idx = 0
-        self.last_break = 0
-        self.lineno = 1
 
     def __iter__(self):
         return self
@@ -28,16 +26,12 @@ class LexerStream(object):
         for rule in self.lexer.ignore_rules:
             match = rule.matches(self.s, self.idx)
             if match:
-                self.__update_lineno__(match.end)                
                 self.idx = match.end
                 return self.next()
         for rule in self.lexer.rules:
             match = rule.matches(self.s, self.idx)
             if match:
-                self.__update_lineno__(match.end)
-                source_pos = SourcePosition(match.start, 
-                                            self.lineno, 
-                                            match.start - self.last_break + 1)
+                source_pos = self.__get_position__(match.start)
                 token = Token(rule.name, self.s[match.start:match.end], source_pos)
                 self.idx = match.end
                 return token
@@ -56,21 +50,20 @@ class LexerStream(object):
         """
 
         old_idx = self.idx
-        old_lineno = self.lineno
-        old_last_break = self.last_break
-
         out = str(list(self))
-
         self.idx = old_idx
-        self.lineno = old_lineno
-        self.last_break = old_last_break
+
         return out
 
-    def __update_lineno__(self, cursor):
-        """ Updates the lineno by counting how many line breaks ('\n') there are 
-        between the current cursor position and the last break. 
-        """
-        assert self.last_break >= 0
-        if self.s.rfind("\n", 0, cursor) >= self.last_break:
-            self.lineno += self.s.count("\n", self.last_break, cursor)
-            self.last_break = self.s.rfind("\n", 0, cursor) + 1
+    def __get_position__(self, cursor):
+        lineno = self.s.count("\n", 0, cursor) + 1
+        
+        colno = cursor + 1
+        if lineno > 1:
+            colno = colno - (self.s.rfind("\n", 0, cursor)+1)
+
+        sp = SourcePosition(cursor, lineno, colno)
+
+        assert colno > 0
+        assert lineno > 0
+        return sp
