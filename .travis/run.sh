ba1f7b1297baf62c430c8ffb7b22138104e63517
Alex Gaynor <alex.gaynor@gmail.com>
2014-08-09 10:24:57 -0700

    Don't try to run pypy under py3k

---
+++ .travis/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -x
+
+case "${TOX_ENV}" in
+    py33)
+        export PYPY_LOCATION=""
+        ;;
+    py34)
+        export PYPY_LOCATION=""
+        ;;
+    *)
+        export PYPY_LOCATION=`python -c "import glob; import os; print os.path.abspath(glob.glob('../pypy-pypy*')[0])"`
+        ;;
+esac
+
+tox -e $TOX_ENV
+
